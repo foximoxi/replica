d9d1d98425baae91a55717aa14b333a7ef512410
Miki <fujiyama@gmail.com>
2018-05-22 21:37:05 +0200

    added missing file

---
+++ Replica/Services/Service/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using X.Config;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace X.Services
+{
+    public interface IService
+    {
+        ILogger Log { get; }
+    }
+}
