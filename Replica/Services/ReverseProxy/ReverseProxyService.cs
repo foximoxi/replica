20fecee8b63c53d1102b927fc5b875485e7fc89c
Miki <fujiyama@gmail.com>
2018-05-23 08:03:03 +0200

    reverse proxy initial stubs

---
+++ Replica/Services/ReverseProxy/ReverseProxyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using X.Config;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace X.Services
+{
+    public class ReverseProxyService:IReverseProxyService
+    {
+        public ILogger Log { get; private set; }
+    }
+}
